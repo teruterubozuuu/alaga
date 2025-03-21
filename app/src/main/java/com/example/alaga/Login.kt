2546d4e993a1182aa9418655a778136481b7965e
dyorgie <tejjlagmay@gmail.com>
2025-03-22 01:55:56 +0800

    added login button and redirect for register

--- app/src/main/java/com/example/alaga/Login.kt
+++ app/src/main/java/com/example/alaga/Login.kt
@@ -3,6 +3,7 @@ package com.example.alaga
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,6 +24,12 @@ class Login : AppCompatActivity() {
 
         backSignin.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val redirectSU : TextView = findViewById(R.id.SUredirect)
+        redirectSU.setOnClickListener{
+            val intent = Intent(this, Register::class.java)
             startActivity(intent)
         }
     }
