db84c8fd45d7a7af25307e2b34947f066e8c9da8
Joylynne Esportuno <degozarunyan@gmail.com>
2025-03-21 16:19:01 +0800

    feat: add welcome screen UI and button navigation to login & signup

--- app/src/main/java/com/example/alaga/Register.kt
+++ app/src/main/java/com/example/alaga/Register.kt
@@ -1,35 +1,20 @@
 package com.example.alaga
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_register)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val signIn : Button = findViewById(R.id.signIn)
-        signIn.setOnClickListener{
-            val intent = Intent(this,Login::class.java)
-            startActivity(intent)
-        }
-
-        val signUp : TextView = findViewById(R.id.signUp)
-        signUp.setOnClickListener{
-            val intent = Intent(this, Register::class.java)
-            startActivity(intent)
         }
     }
 }
