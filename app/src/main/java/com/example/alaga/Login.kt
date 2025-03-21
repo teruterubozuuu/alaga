368433ba63b49e0f580d7080347378c1ae17014d
dyorgie <tejjlagmay@gmail.com>
2025-03-22 00:59:39 +0800

    added back buttons for login and register

--- app/src/main/java/com/example/alaga/Login.kt
+++ app/src/main/java/com/example/alaga/Login.kt
@@ -1,6 +1,8 @@
 package com.example.alaga
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,13 @@ class Login : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val backSignin : Button = findViewById(R.id.SIBackButton)
+
+        backSignin.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
