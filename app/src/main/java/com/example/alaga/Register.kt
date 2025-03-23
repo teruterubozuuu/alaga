516662b52ea6c5680e92e5b6434992622771369b
Joylynne Esportuno <degozarunyan@gmail.com>
2025-03-23 14:29:57 +0800

    feat: add UI on register activity, and button navigation to redirect user to login activity

--- app/src/main/java/com/example/alaga/Register.kt
+++ app/src/main/java/com/example/alaga/Register.kt
@@ -3,6 +3,7 @@ package com.example.alaga
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -23,6 +24,12 @@ class Register : AppCompatActivity() {
 
         backSignup.setOnClickListener{
             val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        val signIn : TextView = findViewById(R.id.SIredirect)
+        signIn.setOnClickListener{
+            val intent = Intent(this, Login::class.java)
             startActivity(intent)
         }
     }
